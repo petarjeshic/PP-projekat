897651927014919f32b2aaa543f8182f8d959e71
petarjeshic <petarjeshic@hotmail.com>
2017-08-27 21:32:20 +0200

    done

---
+++ src/rs/ac/bg/etf/pp1/util/MyCode.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.etf.pp1.util;
+
+import rs.etf.pp1.mj.runtime.Code;
+
+public class MyCode extends Code {
+	public static final int 
+	muleq=44,
+	diveq=45,
+	modeq=46,
+	pluseq=47,
+	minuseq=48,
+	eqeq=61;
+}
